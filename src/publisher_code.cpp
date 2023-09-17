d13904fe306515629ab661ba3f0d703940b63842
Zach Silberstein <zms29@case.edu>
2023-09-17 19:39:58 -0400

    Updated with dependency with Subscriber Package

--- src/publisher_code.cpp
+++ src/publisher_code.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "publisher_package/topic_name.h"
 
 #include <sstream>
 
@@ -44,7 +45,7 @@ int main(int argc, char **argv)
    * than we can send them, the number here specifies how many messages to
    * buffer up before throwing some away.
    */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("publisher_topic", 1000);
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>(topic_name, 1000);
 
   ros::Rate loop_rate(1);
 
